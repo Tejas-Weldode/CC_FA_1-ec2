7776ef98c7739d3a7a22bd3ebb5f3dc29144c415
Tejas Weldode <112164797+Tejas-Weldode@users.noreply.github.com>
2024-09-26 23:57:53 +0530

    changed default port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const cors = require("cors");
 
 // Middleware to parse JSON bodies
